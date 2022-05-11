3139c8f562efa048ee458d1c87c0ed86e643005a
Alex <pf775_ash@rambler.ru>
2022-05-11 20:09:05 +0300

    "ready"

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,27 @@ const autoprefixes = require('gulp-autoprefixer')
 const cleanCSS = require('gulp-clean-css')
 const svgSprite = require('gulp-svg-sprite')
 const image = require('gulp-image')
+const babel = require('gulp-babel')
+const argv = require('yargs').argv;
+const gulpif = require('gulp-if')
+const uglify = require('gulp-uglify-es').default
+const notify = require('gulp-notify')
+const sourcemaps = require('gulp-sourcemaps')
+const del = require('del')
 const browserSync = require('browser-sync').create()
+
+const clean = () => {
+    return del(['dist'])
+}
+
+const resources = () => {
+    return src('src/resources/**')
+        .pipe(dest('dist'))
+}
 
 const styles = () => {
     return src('src/styles/**/*.css')
+        .pipe(gulpif(!argv.production, sourcemaps.init()))
         .pipe(concat('main.css'))
         .pipe(autoprefixes({
             cascade: false
@@ -16,15 +33,16 @@ const styles = () => {
         .pipe(cleanCSS({
             level: 2
         }))
+        .pipe(gulpif(!argv.production, sourcemaps.write()))
         .pipe(dest('dist'))
         .pipe(browserSync.stream())
 }
 
 const htmlMinify = () => {
     return src('src/**/*.html')
-        .pipe(htmlMin({
+        .pipe(gulpif(argv.production, htmlMin({
             collapseWhitespace: true,
-        }))
+        })))
         .pipe(dest('dist'))
         .pipe(browserSync.stream())
 }
@@ -39,6 +57,24 @@ const svgSprites = () => {
             }
         }))
         .pipe(dest('dist/images'))
+}
+
+const scripts = () => {
+    return src([
+        'src/js/components/**/*.js',
+        'src/js/main.js'
+    ])
+    .pipe(gulpif(!argv.production,sourcemaps.init()))
+    .pipe(babel({
+        presets: ['@babel/env']
+    }))
+    .pipe(concat('app.js'))
+    .pipe(gulpif(argv.production, uglify({
+        toplevel: true
+    }).on('error', notify.onError())))
+    .pipe(gulpif(!argv.production,sourcemaps.write()))
+    .pipe(dest('dist'))
+    .pipe(browserSync.stream())
 }
 
 const watchFiles = () => {
@@ -63,7 +99,10 @@ const images = () => {
 watch('src/**/*.html', htmlMinify)
 watch('src/styles/**/*.css', styles)
 watch('src/images/svg/**/*.svg', svgSprites)
+watch('src/js/**/*.js', scripts)
+watch('src/resources/**', resources)
 
 exports.styles = styles
+exports.scripts = scripts
 exports.htmlMinify = htmlMinify
-exports.default = series(htmlMinify, styles, images, svgSprites, watchFiles)
+exports.default = series(clean, resources, htmlMinify, scripts, styles, images, svgSprites, watchFiles)
